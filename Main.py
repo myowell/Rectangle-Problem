8a7fb1158d2b226698661f4de67fd98fb4c3fda2
Michael Yowell <michael.yowell@gmail.com>
2017-08-04 16:13:25 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,12 @@
+'''
+Rectangle Aessessment for Apprenda Inc.
+
+This program will take the coordinates of two rectangles and determine the following:
+- If one or more lines of the two rectangles intersect
+- If one rectangle contains another
+- If the two rectangles share any adjacent lines
+
+@author: Michael Yowell - michael.yowell@gmail.com
+'''
+def main():
+    exit()
