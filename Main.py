9f147bed86db42c5f6e7c8cf8efd1cf49a342cb3
Michael Yowell <michael.yowell@gmail.com>
2017-08-05 05:34:59 -0400

    Began implementation of GUI

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 '''
-Rectangle Aessessment for Apprenda Inc.
+Main method for the rectangle aessessment for Apprenda Inc.
 
 This program will take the coordinates of two rectangles and determine the following:
 - If one or more lines of the two rectangles intersect
@@ -11,9 +11,11 @@ This program will take the coordinates of two rectangles and determine the follo
 
 from classes.rectangle import Rectangle
 from classes.coordinate import Coordinate
+from Tkinter import *
 
 if __name__ == '__main__':
-    rect1 = Rectangle(Coordinate(1, 5), Coordinate(4, 5) , Coordinate(2, 3), Coordinate(4, 3))
-    rect2 = Rectangle(Coordinate(1, 3), Coordinate(4, 3) , Coordinate(1, 1), Coordinate(4, 1))
-    print(rect1.rectangleAdjacent(rect2))
-    print(rect2.rectangleAdjacent(rect1))
+    master = Tk()
+
+    
+    
+    master.mainloop()
