41ec1ea62dedf0c26bb5ed5ead4bfc3ce6ef5a2c
Michael Yowell <michael.yowell@gmail.com>
2017-08-04 21:34:40 -0400

    Implemented test for intersection

--- Main.py
+++ Main.py
@@ -13,6 +13,6 @@ from classes.rectangle import Rectangle
 from classes.coordinate import Coordinate
 
 if __name__ == '__main__':
-    rect1 = Rectangle(Coordinate(3, 6), Coordinate(7, 6) , Coordinate(3, 3), Coordinate(7, 3))
-    rect2 = Rectangle(Coordinate(4, 7), Coordinate(8, 7) , Coordinate(4, 4), Coordinate(8, 4))
-    print(rect1.rectangleContains(rect2))
+    rect1 = Rectangle(Coordinate(1, 5), Coordinate(3, 5) , Coordinate(1, 2), Coordinate(3, 2))
+    rect2 = Rectangle(Coordinate(1, 8), Coordinate(3, 8) , Coordinate(1, 7), Coordinate(3, 7))
+    print(rect1.rectangleIntersects(rect2))
--- classes/rectangle.py
+++ classes/rectangle.py
@@ -2,9 +2,10 @@ from coordinate import Coordinate
 
 class Rectangle(object):
     '''
-    Rectangle class for the Apprenda rectangle assessment
+    Rectangle class for the Apprenda rectangle assessment.
     
-    Contains the four coordinates of a rectangle
+    Contains the four coordinates of a rectangle.
+    Defines the methods used for determining if two rectangles intersect, if one rectangle is contained within another, and if two rectangles are adjacent.
     
     Attributes:
         upperLeft: The upper left coordinate.
@@ -56,16 +57,15 @@ class Rectangle(object):
         Determines if a rectangle is valid
         
         A rectangle is valid iff it meets the following criteria:
-        ALL x and y coordinates are greater than zero
-        The upper left's x-coordinate equals the x-coordinate of the bottom left and its y-coordinate matches the y-coordinate of the top right
-        The bottom left's x-coordinate equals the y-coordinate of the bottom right
-        The bottom right's x-coordinate equals the x-coordinate of the top right
+        ALL x and y coordinates are greater than zero.
+        The upper left's x-coordinate equals the x-coordinate of the bottom left and its y-coordinate matches the y-coordinate of the top right.
+        The bottom left's x-coordinate equals the y-coordinate of the bottom right.
+        The bottom right's x-coordinate equals the x-coordinate of the top right.
         
-        Return true if a rectangle is valid
+        Return true if a rectangle is valid.
         
-        Return false if a rectangle is invalid
-        '''
-        
+        Return false if a rectangle is invalid.
+        '''      
         if self.getUpperLeft().getX() == self.getBottomLeft().getX() \
         and self.getUpperLeft().getY() == self.getUpperRight().getY() \
         and self.getBottomLeft().getX() == self.getBottomRight().getY() \
@@ -73,18 +73,41 @@ class Rectangle(object):
             return True
         else:
             return False
+    
+    def rectangleIntersects(self, rectangleTwo):
+        '''
+        Determines if this rectangle intersects another.
+        
+        Two rectangles are considered intersecting if any of their coordinates overlap at one or more points.
+        
+        Return true if the two rectangles intersect.
+        
+        Return false if the two rectangles do not intersect.
+        
+        Attributes:
+            rectangleTwo: A instance of the Rectangle() class
+        '''      
+        if (self.getUpperLeft().getX() > rectangleTwo.getBottomRight().getX() or rectangleTwo.getUpperLeft().getX() > self.getBottomRight().getX()) \
+        or (self.getUpperLeft().getY() < rectangleTwo.getBottomRight().getY() or rectangleTwo.getUpperLeft().getY() < self.getBottomRight().getY()):
+            return False
+        else:
+            return True;
         
     def rectangleContains(self, rectangleTwo):
         '''
-        Determines if this rectangle is contained within another
+        Determines if this rectangle is contained within another.
         
-        A rectangle is considered contained iff the x and y-coordinates for vertex are less than or equal to the vertices of another rectangle
+        A rectangle is considered contained iff the x and y-coordinates for each vertex are less than or equal to the vertices of another rectangle.
         
-        Return true if this rectangle is contained within rectangleTwo
+        Return true if this rectangle is contained within rectangleTwo.
         
-        Return false if this rectangle is not contained within rectangleTwo
-        '''
+        Return false if this rectangle is not contained within rectangleTwo.
         
+        Attributes:
+            rectangleTwo: A instance of the Rectangle() class.
+        '''    
+        
+           
         if self.getUpperLeft().getX() <= rectangleTwo.getUpperLeft().getX() and self.getUpperLeft().getY() <= rectangleTwo.getUpperLeft().getY() \
         and self.getUpperRight().getX() <= rectangleTwo.getUpperRight().getX() and self.getUpperRight().getY() <= rectangleTwo.getUpperRight().getY() \
         and self.getBottomLeft().getX() <= rectangleTwo.getBottomLeft().getX() and self.getBottomLeft().getY() <= rectangleTwo.getBottomLeft().getY() \
